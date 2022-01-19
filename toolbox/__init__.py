f4c1931fbf9f21140cf02a04b2d159badf9f0ae8
guzman-raphael <raphael.h.guzman@gmail.com>
2022-01-19 14:59:43 -0600

    Fix test.

--- toolbox/__init__.py
+++ toolbox/__init__.py
@@ -1,3 +1,4 @@
 from .utils import msg_formatter
 
+__all__ = ['msg_formatter']
 __version__ = '0.0.0'
--- toolbox/utils.py
+++ toolbox/utils.py
@@ -2,4 +2,4 @@ from datetime import datetime
 
 
 def msg_formatter(s):
-    return f'[{datetime.now().isoformat()}]: {s}'
+    return f'[{datetime.now().isoformat()}][toolbox]: {s}'
