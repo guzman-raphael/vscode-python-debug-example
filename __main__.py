28bae011078b68f92e37db60540dc5b4e20e12ce
guzman-raphael <raphael.h.guzman@gmail.com>
2022-01-19 15:39:48 -0600

    Add debugger example invoking a method.

--- __main__.py
+++ __main__.py
@@ -1,8 +1,19 @@
-a = 2
-b = 3
+# === 2022-01-19 10AM ===
+# a = 2
+# b = 3
 
-c = a + 2 * b
+# c = a + 2 * b
 
-d = c - b
+# d = c - b
 
-print(d)
+# print(d)
+
+# === 2022-01-19 1PM ===
+
+import toolbox
+
+msg = toolbox.msg_formatter('now')
+
+msg_final = f'{msg}!'
+
+print(msg_final)
